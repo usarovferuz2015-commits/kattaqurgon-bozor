52bca597f5c2e0ca3ff1b574c4859d6d8ef7e7e5
Feruz Usarov <usarov.feruz2015@gmail.com>
2026-05-26 22:07:17 +0500

    Remove legacy plugin, keep es2015 target

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+  plugins: [react()],
   build: {
     target: 'es2015',
   },
